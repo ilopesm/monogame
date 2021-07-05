9d7a32754c58d222e46983a08120f20d86c50096
ilopesm <isaaclopes11@hotmail.com>
2021-07-05 02:58:02 -0300

    iniciando o projeto

---
+++ tdd-jogodavelha/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace tdd_jogodavelha
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
